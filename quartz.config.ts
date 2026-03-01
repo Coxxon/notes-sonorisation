d27c37adaebe554aca40c0c541ba7b1a9931750a
Coxxon <allan.lg@outlook.fr>
2026-03-01 10:46:52 +0100

    Quartz sync: Mar 1, 2026, 10:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Syntax",
+    pageTitle: "SYNTAX",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
