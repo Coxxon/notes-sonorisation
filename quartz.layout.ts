8972f24a88e833c78eda071b92ed66709a19c486
Coxxon <allan.lg@outlook.fr>
2026-03-02 01:35:05 +0100

    Quartz sync: Mar 2, 2026, 1:35 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,12 +8,12 @@ export const sharedPageComponents: SharedLayout = {
     Component.MobileOnly(Component.MobileMenu({ // Menu hamburger à gauche
       recentNotesTitle: "Fondamentaux",
       recentNotesLimit: 10,
-      recentNotesFilter: (f: any) => Boolean(f.frontmatter?.tags?.includes("fondamentaux")),
-      recentNotesSort: (f1: any, f2: any) => Number((f1.frontmatter?.order ?? 100)) - Number((f2.frontmatter?.order ?? 100)),
+      recentNotesFilter: (f: any) => f.frontmatter?.tags?.includes("fondamentaux") === true,
+      recentNotesSort: (f1: any, f2: any) => (f1.frontmatter?.order ?? 100) - (f2.frontmatter?.order ?? 100),
       recentNotesShowTags: false,
     })),
+    Component.MobileOnly(Component.Search()),   // Recherche à droite du menu hamburger
     Component.MobileOnly(Component.PageTitle()), // SYNTAX au centre (uniquement mobile)
-    Component.MobileOnly(Component.Search()),   // Loupe à droite du titre
     Component.MobileOnly(Component.Darkmode()), // Darkmode à droite
   ],
   afterBody: [],
@@ -33,7 +33,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(
       Component.Flex({
         components: [
+          { Component: Component.Search(), grow: true },
           { Component: Component.Darkmode() },
+          { Component: Component.ReaderMode() },
         ],
       })
     ),
@@ -64,6 +66,10 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(
       Component.Flex({
         components: [
+          {
+            Component: Component.Search(),
+            grow: true,
+          },
           { Component: Component.Darkmode() },
         ],
       })
